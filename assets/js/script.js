11af91a0f21c0797a807e85613738f8be32029ed
Ashley <ashleyh5440@gmail.com>
2024-01-03 20:31:00 -0500

    changed background and images

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+const contactBtn = document.getElementById("y-button")
+contactMeSection = document.getElementById('contact-me');
+
+contactBtn.addEventListener('click', function() {
+    contactMeSection.classList.remove('hidden');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
