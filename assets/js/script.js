39105c65dc1b5c84fd998fd8d577ab1dd03eba55
Ashley <ashleyh5440@gmail.com>
2024-01-03 22:06:41 -0500

    added hidden classes and back button, need to fix the js to make these function correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-const contactBtn = document.getElementById("y-button")
-contactMeSection = document.getElementById('contact-me');
+const yBtn = document.getElementById("y-button");
+const aBtn = document.getElementById("a-button");
+const bBtn = document.getElementById("b-button");
+const backBtn = document.getElementById("back-button");
 
-contactBtn.addEventListener('click', function() {
-    contactMeSection.classList.remove('hidden');
-})
+const aboutInfo = document.getElementById("about-me");
+const worksInfo = document.getElementById("my-works");
+const contactInfo = document.getElementById("contact-me");
+
+const baseImg = document.getElementById("header-img")
+
+yBtn.addEventListener('click', function() {
+    backBtn.classList.remove("hidden");
+    contactInfo.classList.remove("hidden");
+    baseImg.classList.add("hidden")
+});
+
+aBtn.addEventListener('click', function() {
+    backBtn.classList.remove("hidden");
+    aboutInfo.classList.remove("hidden");
+    baseImg.classList.add("hidden")
+});
+
+bBtn.addEventListener('click', function() {
+    backBtn.classList.remove("hidden");
+    worksInfo.classList.remove("hidden");
+    baseImg.classList.add("hidden")
+});
+
+backBtn.addEventListener('click', function() {
+    baseImg.classList.remove("hidden")
+    contactInfo.classList.add("hidden");
+    aboutInfo.classList.add("hidden");
+    worksInfo.classList.add("hidden");
+});
 
 
 
